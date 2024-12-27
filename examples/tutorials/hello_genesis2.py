f3aa67f1c35cdcb1ab5fde9ac02c9ba15f344673
Sergio Ortiz <63668386+sortiz-hub@users.noreply.github.com>
2024-12-27 19:15:19 +0000

    tedst

--- examples/tutorials/hello_genesis2.py
+++ examples/tutorials/hello_genesis2.py
@@ -3,7 +3,7 @@ import genesis as gs
 gs.init(backend=gs.cpu)
 
 scene = gs.Scene(
-    show_viewer    = True,
+    show_viewer = False,
     viewer_options = gs.options.ViewerOptions(
         res           = (1280, 960),
         camera_pos    = (3.5, 0.0, 2.5),
@@ -12,40 +12,44 @@ scene = gs.Scene(
         max_FPS       = 60,
     ),
     vis_options = gs.options.VisOptions(
-        show_world_frame = True, # visualize the coordinate frame of `world` at its origin
-        world_frame_size = 1.0, # length of the world frame in meter
-        show_link_frame  = False, # do not visualize coordinate frames of entity links
-        show_cameras     = False, # do not visualize mesh and frustum of the cameras added
-        plane_reflection = True, # turn on plane reflection
-        ambient_light    = (0.1, 0.1, 0.1), # ambient light setting
+        show_world_frame = True,
+        world_frame_size = 1.0,
+        show_link_frame  = False,
+        show_cameras     = False,
+        plane_reflection = True,
+        ambient_light    = (0.1, 0.1, 0.1),
     ),
-    renderer = gs.renderers.Rasterizer(), # using rasterizer for camera rendering
+    renderer=gs.renderers.Rasterizer(),
 )
 
 plane = scene.add_entity(
     gs.morphs.Plane(),
 )
 franka = scene.add_entity(
-    # gs.morphs.URDF(
-    #     file='urdf/panda_bullet/panda.urdf',
-    #     fixed=True,
-    # ),
-    gs.morphs.MJCF(file="xml/franka_emika_panda/panda.xml"),
+    gs.morphs.MJCF(file='xml/franka_emika_panda/panda.xml'),
 )
 
 cam = scene.add_camera(
-    res    = (1280, 960),
+    res    = (640, 480),
     pos    = (3.5, 0.0, 2.5),
     lookat = (0, 0, 0.5),
     fov    = 30,
-    GUI    = False
+    GUI    = False,
 )
 
 scene.build()
+
+# render rgb, depth, segmentation, and normal
+# rgb, depth, segmentation, normal = cam.render(rgb=True, depth=True, segmentation=True, normal=True)
+
 cam.start_recording()
+import numpy as np
 
-for i in range(1000):
+for i in range(120):
     scene.step()
+    cam.set_pose(
+        pos    = (3.0 * np.sin(i / 60), 3.0 * np.cos(i / 60), 2.5),
+        lookat = (0, 0, 0.5),
+    )
     cam.render()
-
 cam.stop_recording(save_to_filename='video.mp4', fps=60)
